371210031e86f695f133bf522dd6c9ade8a63057
Damian <G00419511@atu.ie>
2023-11-21 13:27:53 +0000

    Added new code into class file to create a new File into a folder

--- src/main/java/atu/ie/CreateFile.java
+++ src/main/java/atu/ie/CreateFile.java
@@ -2,9 +2,26 @@ package atu.ie;
 
 import java.io.File;
 import java.io.IOException;
+
 public class CreateFile {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        // Specify the file name
+        String fileName = "File13.txt";
 
+        // Create a File object
+        File myFile = new File(fileName);
+
+        try {
+            // Create a new file on disk
+            if (myFile.createNewFile()) {
+                System.out.println("File created successfully at " + myFile.getAbsolutePath());
+            } else {
+                System.out.println("File already exists at " + myFile.getAbsolutePath());
+            }
+        } catch(IOException e){
+                System.out.println("An error occurred while creating new file. ");
+                e.printStackTrace();
+        }
     }
 }
