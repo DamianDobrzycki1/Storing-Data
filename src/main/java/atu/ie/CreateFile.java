d5b24f5a206ae79c5678401aa6c68f0b42ed7523
Damian <G00419511@atu.ie>
2023-11-21 13:53:11 +0000

    Added more code into CreateFile class

--- src/main/java/atu/ie/CreateFile.java
+++ src/main/java/atu/ie/CreateFile.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 
 public class CreateFile {
-
     public static void main(String[] args) {
         // Specify the file name
         String fileName = "File13.txt";
