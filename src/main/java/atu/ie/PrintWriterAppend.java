222cb3756f9c745bf1cbfa9f878163dcf3f6708e
Damian <G00419511@atu.ie>
2023-11-21 14:44:39 +0000

    Added more code

--- src/main/java/atu/ie/PrintWriterAppend.java
+++ src/main/java/atu/ie/PrintWriterAppend.java
@@ -3,10 +3,22 @@ package atu.ie;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class PrintWriterAppend {
 
     public static void main(String[] args){
 
+        String fileName = "File13.txt";
+
+        try(PrintWriter writer = new PrintWriter(new FileWriter(fileName, true))) {
+            writer.println("This line will be appended.");
+            writer.printf("Appended formatted content: %d %s%n", 42, "example");
+
+            System.out.println("Content successfully appended to the file.");
+        }catch(IOException e){
+            System.out.println("Could not Append the content");
+            e.printStackTrace();
+        }
     }
 }
